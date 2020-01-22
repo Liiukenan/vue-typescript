11753c088113cc5a33347d04e38862ceee6410ab
liukenan <liukenan@lbesec.com>
2020-01-22 17:14:49 +0800

    测试typescript在vue上的应用

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,46 @@
 <template>
 <div class="about">
-
+  {{message | addword}}
+  {{name}}
+  {{lastName}}
+  {{greet()}}
+  {{message}}
+  {{mes}}
+  {{child}}
 </div>
 </template>
 <script lang="ts">
-  import { Component, Vue } from "vue-property-decorator";
-  @Component
+  import { Component, Vue,Prop, Watch} from "vue-property-decorator";
+  @Component({
+    filters:({
+      addword(value:string){
+        return "world";
+      }
+    })
+  })
   export default class HelloWorld extends Vue {
+    // data值
+    name:string="kenan";
+    // 钩子
     mounted(){
       const hello : string = "Hello2 World!"
       console.log(hello);
+    };
+    // 计算属性
+    get lastName(){
+      return 'liu';
+    };
+    // 方法
+    greet(){
+        return "liyoucun"
+    }
+    @Prop({type:String})
+    message:any
+    @Prop({default:'没有东西了'})
+    mes:any
+    @Watch('child')
+    onChildChanged(){
+      return '这是watch'
     }
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <About message="Welcome to Your Vue.js App2" mes="1234" />
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import About from "@/views/About.vue";
 import { Component, Vue } from "vue-property-decorator";
   @Component({
     components:{
-      HelloWorld
+      HelloWorld,
+      About
     }
   })
   export default class Home extends Vue {
